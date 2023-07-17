070c5c77d25063b77cfbe946505985eff9b69b9d
Pronin Mike <mr.mi@mail.ru>
2023-07-17 23:47:30 +0400

    JavaOopHW4.1

---
+++ Lectures4/HW/Calculator.java
@@ -0,0 +1,45 @@
+package Lectures4.HW;
+
+import java.util.List;
+
+public class Calculator<T> {
+    /**
+     * @param numbers массив чисел
+     * @return сумма всех элементов массива
+     * @apiNote 3)	Написать класс калькулятор, принимающий List целочисленных значений,
+     * возвращающий сумму элементов коллекции (метод sum)
+     */
+    public Double sum(List<? extends Number> numbers) {
+        Double res = 0.0;
+        for (Number number : numbers) {
+            res += number.doubleValue();
+        }
+        return res;
+    }
+    public Double multi(List<? extends Number> numbers) {
+        Double res = 1.0;
+        for (Number number : numbers) {
+            res *= number.doubleValue();
+        }
+        return res;
+    }
+    public Double div(List<? extends Number> numbers) {
+        Double res = 1.0;
+        for (Number number : numbers) {
+            res /= number.doubleValue();
+        }
+        return res;
+    }
+    public Integer binary(T number) {
+        String num = String.valueOf(number).replace(".0","");
+        Integer quotient = Integer.valueOf(num);
+        StringBuilder result = new StringBuilder();
+        while (quotient > 0) {
+            Integer remainder = quotient % 2;
+            result.append(remainder);
+            quotient /= 2;
+        }
+        result = result.reverse();
+        return Integer.valueOf(result.toString());
+    }
+}
